b289af6abcdd87e3589bd80d8a6b9a8773333646
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-03-10 22:08:38 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,38 @@
+//Extract the first four characters from the string below;
+//"beautiful"
+let a="Beautiful";
+console.log (a[0]);
+console.log (a[1]);
+console.log (a[2]);
+console.log (a[3]);
+
+//Insert the following string at the tenth index of the below variable:
+//"eat"
+//const food = "I did not have appetite today"
+const food="I did not have an appetite today"
+const food="eat"
+console.log(food)
+//Count how many times the following string appears in the string variable:
+//1. "the"
+//2."s"
+//const story= "She sells sea shells at the sea shore"
+const story="She sells sea shells at the sea shore"
+let s=([0][3][7][8][11][16][22][25])
+console.log(s)
+
+//Convert the following strings into the specified format:
+//1. UpperCase: "CONfidant"
+//2. LowerCase: "amazing", "beautiFUL"
+//3. Title case "A busy office"
+//Using JavaScript, find the following words from the following strings:
+//1. "market"
+//const string1 = "The lady went to the market with her sister";
+//2. "season"
+//const string2 = "My favorite season is spring"
+//let a="Beautiful";
+//console.log (a.slice(0));
+//console.log (a.slice(1));
+//console.log (a.slice(2));
+//console.log (a.slice(3));
+
+//const food="I did not have an appetite today"
